42406dff7d75ad9e62909134d16640ebfd161604
Alex <alkizer@microsoft.com>
2019-07-23 20:56:27 -0700

    changes

--- grammarParser.ts
+++ grammarParser.ts
@@ -14,11 +14,25 @@ const builtInTerminals = {
     '<newline>': {
         type: `literal`,
         name: `NewLine`,
-        value: `\n`
+        value: `\n`,
+        parseFn: (source, index, scout) => {
+            if (source[index + scout] === `\n`) {
+                scout++;
+                return true;
+            }
+            return false;
+        }
     },
     '<eof>': {
         type: `eof`,
-        name: `EndOfFile`
+        name: `EndOfFile`,
+        value: null,
+        parseFn: (source, index, scout) => {
+            if (index === source.length) {
+                return true;
+            }
+            return false;
+        }
     }
 };
 
@@ -27,24 +41,30 @@ const grammarSource: string = fs.readFileSync(`./language.grammar`, `utf8`),
         productions: []
     };
 
-/* parse `language.grammar` file and populate `grammar` object */
 let currentProduction = null,
     currentDerivations = [],
     lines = grammarSource.split(`\n`);
+
+const pushCurrentProduction = () => {
+    grammar.productions.push({
+        name: currentProduction,
+        derivations: Array.from(currentDerivations)
+    });
+    currentProduction = null;
+    currentDerivations = [];
+}
   
+/* parse `language.grammar` file and populate `grammar` object */
 lines.map((line, i) => {
+
+    debugMode && console.log(`${i}: ${line}`);
     
-    if (/^[a-z]+$/.test(line.trim()) || i === lines.length - 1) {
+    if (/^[A-Za-z]+$/.test(line.trim())) {
                 
         /* new production */
         
         if (currentProduction) {
-            grammar.productions.push({
-                name: currentProduction,
-                derivations: Array.from(currentDerivations)
-            });
-            currentProduction = null;
-            currentDerivations = [];
+            pushCurrentProduction();
         }
         
         currentProduction = line.trim();
@@ -52,34 +72,88 @@ lines.map((line, i) => {
         
         /* new derivation for current production */
        
+        let to = [];
+
         line.replace(`    |`, ``).trim().split(` `).map((target) => {
+            debugMode && console.log(`   [target] ${target}`);
             if (target.startsWith(`"`)) {
                 let raw = target.replace(/"/g, ``);
-                currentDerivations.push({
+                to.push({
                     type: `literal`,
                     name: `Literal_${raw.replace(/ /g, `_`)}`,
                     value: raw
                 });
             } else if (target.startsWith(`<`)) {
-                currentDerivations.push(builtInTerminals[target]);
+                to.push(builtInTerminals[target]);
             } else {
-                currentDerivations.push({
+                to.push({
                     type: `nonterminal`,
                     productionName: target
                 });
             }
         });
+        
+        currentDerivations.push(to);
+    } else if (line === `\n` || line === ``) {
+        // ignore
+    } else {
+        // TODO
+        //debugMode && console.log(`Bad line:`);
+        //debugMode && console.log(line);
     }
 });
 
-debugMode && console.log(JSON.stringify(grammar, null, 4)); // ifs are for losers
+if (currentProduction) {
+    pushCurrentProduction();
+}
+
+debugMode && console.log(JSON.stringify(grammar, null, 4));
 
 /* generate a parser for the language */
+let sc = ``, // source code
+    { productions } = grammar,
+    scs = {
+        successfulParse: `/* success */ index += scout; scout = 0; return true;`,
+        failedParse: `/* fail */ scout = 0; return false;`,
+    };
 
-let sc = ``,
-    { productions } = grammar;
+const getParseFnName = (productionName) => `parse${productionName}`;
 
-productions.map((production) => {
-    sc += `const parse${production.name} = () => {\n`;
+sc += `// generated ${new Date()}\n\n`;
+sc += `let source, index = 0, scout = 0;\n\n`;
+
+Object.values(builtInTerminals).map((b) => {
+    if (b.parseFn) {
+        sc += b.parseFn;
+    }
 });
 
+productions.map((production) => {
+    //sc += `/*\n * `;
+    //sc +=  JSON.stringify(production, null, 4).split(`\n`).join(`\n * `);
+    //sc += `\n*/\n`;
+    sc += `const ${getParseFnName(production.name)} = () => {\n`;
+    sc += `if (index >= source.length) return false;\n`
+    production.derivations.map((derivation) => {
+        sc += `    if (\n`;
+        let clauses = [];
+        derivation.map((to) => {
+            if (to.type === `literal` || to.type === `eof`) {
+                clauses.push(`parse${to.name}()`);
+            } else if (to.type === `nonterminal`) {
+                clauses.push(`parse${to.productionName}()`);
+            } else {
+                debugMode && console.log(`Invalid derivation:`)
+                debugMode && console.log(JSON.stringify(to, null, 4));
+            }
+        });
+        sc += `        ` + clauses.join(` &&\n        `);
+        sc += `\n    ) {\n        ${scs.successfulParse}\n    }\n`;
+    });
+    sc += `    ${scs.failedParse}\n`;
+    sc += `};\n\n`;
+});
+
+sc += `// end generated code`;
+
+debugMode && console.log(sc);
