fbbe823c4a3f327e884eb45090107a246d321734
Alex <alkizer@microsoft.com>
2019-08-01 16:27:56 -0700

    begin refactor

--- grammarParserGen.ts
+++ grammarParserGen.ts
@@ -77,102 +77,112 @@ const builtInTerminals = {
     }
 };
 
-const grammarSource: string = fs.readFileSync(`./language.kzrgrammar`, `utf8`),
-    grammar = {
-        productions: []
-    };
+const grammarSource: string = fs.readFileSync(`./language.kzrgrammar`, `utf8`);
 
-let currentProduction = null,
-    currentDerivations = [],
-    lines = grammarSource.replace(/;[^\n]*\n/g, `\n`).split(`\n`);
+const lines = grammarSource.replace(/;[^\n]*\n/g, `\n`).split(`\n`);
 
-const pushCurrentProduction = () => {
+const pushCurrentProduction = (grammar, ctx) => {
     grammar.productions.push({
-        name: currentProduction,
-        derivations: Array.from(currentDerivations),
-        isEntryProduction: currentProduction.trim().startsWith(`$`)
+        name: ctx.currentProduction,
+        derivations: Array.from(ctx.currentDerivations),
+        isEntryProduction: ctx.currentProduction.trim().startsWith(`$`)
     });
-    currentProduction = null;
-    currentDerivations = [];
+    ctx.currentProduction = null;
+    ctx.currentDerivations = [];
 }
 
-/* parse `language.grammar` file and populate `grammar` object */
-lines.map((line, i) => {
+const parseLines = (lines) => {
+    const grammar = {
+        productions: []
+    },
+    ctx = {
+        currentProduction: null,
+        currentDerivations: []
+    };
+    /* parse `language.grammar` file and populate `grammar` object */
+    lines.map((line, i) => {
 
-    debugMode && console.log(`${i}: ${line}`);
-    
-    if (/^[$A-Za-z]+$/.test(line.trim())) {
-                
-        /* new production */
+        debugMode && console.log(`${i}: ${line}`);
         
-        if (currentProduction) {
-            pushCurrentProduction();
-        }
-        
-        currentProduction = line.trim();
-    } else if (line.startsWith(`    |`)) {
-        
-        /* new derivation for current production */
-       
-        let to = [];
-
-        line.replace(`    |`, ``).trim().split(` `).map((target) => {
-            debugMode && console.log(`   [target] ${target}`);
-
-            // handle presence target
-            let matchResult = null,
-              isPresence = false;
-            if (matchResult = target.match(/\(([^\)]+)\)/)) {
-              debugMode && console.log(`presence target ${target} detected`);
-              target = target.replace(/\(([^\)]+)\)/, matchResult[1]);
-              isPresence = true;
-            }
-
-            // handle quantifier
-            let q = null;
-            if (q = target.match(/{([*|+])}/)) {
-                debugMode && console.log(`quantifier ${q[1]}`);
-                q = q[1];
-            }
-            target = target.replace(/{([*|+])}/, ``);
+        if (/^[$A-Za-z]+$/.test(line.trim())) {
+                    
+            /* new production */
             
-            // literal, non-terminal, built-in?
-            if (target.startsWith(`"`)) {
-                let raw = target.replace(/"/g, ``);
-                to.push({
-                    type: `literal`,
-                    name: `Literal_${raw.replace(/ /g, `_`)}`,
-                    value: raw,
-                    quantifier: q,
-                    isPresence
-                });
-            } else if (target.startsWith(`<`)) {
-                to.push(Object.assign({}, builtInTerminals[target], { quantifier: q }));
-            } else {
-                to.push({
-                    type: `nonterminal`,
-                    productionName: target,
-                    quantifier: q,
-                    isPresence
-                });
+            if (ctx.currentProduction) {
+                pushCurrentProduction(grammar, ctx);
             }
-        });
+           
+            ctx.currentProduction = line.trim();
+        } else if (line.startsWith(`    |`)) {
+            
+            /* new derivation for current production */
+        
+            let to = [];
 
-        currentDerivations.push(to);
-    } else if (line === `\n` || line === ``) {
-        // ignore
-    } else {
-        // TODO
-        //debugMode && console.log(`Bad line:`);
-        //debugMode && console.log(line);
+            line.replace(`    |`, ``).trim().split(` `).map((target) => {
+                debugMode && console.log(`   [target] ${target}`);
+
+                // handle presence target
+                let matchResult = null,
+                isPresence = false;
+                if (matchResult = target.match(/\(([^\)]+)\)/)) {
+                debugMode && console.log(`presence target ${target} detected`);
+                target = target.replace(/\(([^\)]+)\)/, matchResult[1]);
+                isPresence = true;
+                }
+
+                // handle quantifier
+                let q = null;
+                if (q = target.match(/{([*|+])}/)) {
+                    debugMode && console.log(`quantifier ${q[1]}`);
+                    q = q[1];
+                }
+                target = target.replace(/{([*|+])}/, ``);
+                
+                // literal, non-terminal, built-in?
+                if (target.startsWith(`"`)) {
+                    let raw = target.replace(/"/g, ``);
+                    to.push({
+                        type: `literal`,
+                        name: `Literal_${raw.replace(/ /g, `_`)}`,
+                        value: raw,
+                        quantifier: q,
+                        isPresence
+                    });
+                } else if (target.startsWith(`<`)) {
+                    to.push(Object.assign({}, builtInTerminals[target], { quantifier: q }));
+                } else {
+                    to.push({
+                        type: `nonterminal`,
+                        productionName: target,
+                        quantifier: q,
+                        isPresence
+                    });
+                }
+            });
+
+            ctx.currentDerivations.push(to);
+        } else if (line === `\n` || line === ``) {
+            // ignore
+        } else {
+            // TODO
+            //debugMode && console.log(`Bad line:`);
+            //debugMode && console.log(line);
+        }
+    });
+
+    if (ctx.currentProduction) {
+        pushCurrentProduction(grammar, ctx);
     }
-});
+    
+    debugMode && console.log(JSON.stringify(grammar, null, 4));
+    
+    return grammar;
+};
 
-if (currentProduction) {
-    pushCurrentProduction();
-}
+let grammar = parseLines(lines);
 
-debugMode && console.log(JSON.stringify(grammar, null, 4));
+
 
 /* generate a parser for the language */
 let sc = ``, // source code
